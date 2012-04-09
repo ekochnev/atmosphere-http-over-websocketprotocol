16418fa0d957a3c0785e73c5d1bfc830d635b7c9
unknown <Jeny@jeny.(none)>
2012-04-09 12:29:16 -0400

    initial

--- core/modules/httpoverwebsocketprotocol/src/main/java/com/jeny/atmosphere/integration/websocket/JsonWebSocketProtocol.java
+++ core/modules/httpoverwebsocketprotocol/src/main/java/com/jeny/atmosphere/integration/websocket/JsonWebSocketProtocol.java
@@ -13,6 +13,7 @@
 * License for the specific language governing permissions and limitations under
 * the License.
 */
+
 package com.jeny.atmosphere.integration.websocket;
 
 import com.google.gson.Gson;
